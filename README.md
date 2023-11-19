b65f57325e332af67639fc25810bad43a79ddc66
HoangPhong <macos@HoangPhong.local>
2023-11-19 23:35:02 +0700

    initial

--- README.md
+++ README.md
@@ -1,70 +1,27 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# Project, Version
+- name: Calculator 
+- version: 
+    - npm: 10.2.1
+    - react: 18.2.0
+    - react-dom: 18.2.0
+- install project:
+    => Run the command npx create-react-app project_name
+- Project: 
+    - Evaluate the correctness or incorrectness of a basic mathematical expression that the user enters from the given keys
+    
+# Run app
+- run app:
+    - Run the command: npm run start
+    - Click 'START'
+    - The game has 10 questions, 1 correct answer gets 10 points, wrong gets 0 points
+    - Enter expressions using numbers 0-9.
+    - Click 'C' to reset the screen and perform a new check
+    - Click 'SUBMIT' to check and receive the results
+    - Click '>>' to go to the next question
+    - Each question has 30 seconds to SUBMIT
+    - Click 'Finish' to exit
+- Reward: 
+    - Score > 80/100: Gold medal
+    - Score > 70/100: Silver medal
+    - Score > 50/100: Bronze medal
+    - Score < 60 /100: No
--- src/App.js
+++ src/App.js
@@ -1,24 +1,250 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable eqeqeq */
+/* eslint-disable no-eval */
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [isStart, setIsStart] = useState(false)
+  const [complete, setComplete] = useState(false)
+  const [achives, setAchives] = useState(0)
+  const [questionTime, setQuestionTime] = useState(60)
+  const [questionOperator, setQuestionOperator] = useState('random')
+  let achive = 0
+
+  // Component Home
+  function Home() {
+    const handleQuestionTime = (e) => {
+      const value = e.target.value
+      setQuestionTime(value)
+    }
+
+    const handleQuestionOperator = (e) => {
+      const value = e.target.value
+      setQuestionOperator(value)
+    }
+
+    return (
+      <>
+        <div className='box-mode'>
+          <h2>Mời chọn chế độ chơi</h2>
+          <div className='mode'>
+            <div className='mode-time'>
+              <label htmlFor='level-time'>Chọn thời gian:</label>
+              <select id='level-time'
+
+                value={questionTime}
+                onChange={handleQuestionTime}>
+                <option value='10'>10s/question</option>
+                <option value='20'>20s/question</option>
+                <option value='60'>60/question</option>
+                <option value='180'>180/question</option>
+              </select>
+            </div>
+            <div className='mode-operator'>
+              <label htmlFor='operator'>Chọn phép toán:</label>
+              <select id='operator'
+                value={questionOperator}
+                onChange={handleQuestionOperator}
+              >
+                <option value='random'>Ngẫu nhiên</option>
+                <option value='+'>Cộng (+)</option>
+                <option value='-'>Trừ (-)</option>
+                <option value='*'>Nhân (*)</option>
+                <option value='/'>Chia (/)</option>
+              </select>
+            </div>
+
+
+          </div>
+        </div>
+        <button
+          type='button'
+          className='start button-main'
+          onClick={() => setIsStart(!isStart)}
+        >Start</button>
+        <div className='frame'>
+        </div>
+      </>
+    )
+  }
+
+  // Component Complete
+  function Complete() {
+    let medal = 'medal'
+    let note = 'note'
+    let score = `Thành tích: ${achives}/100`
+    let notes = 'Chưa đủ để nhận thưởng'
+    if (achives < 60) {
+      medal = 'medal'
+      note = 'note medal4'
+    }
+    if (achives > 50) {
+      medal = 'medal medal3'
+    }
+    if (achives > 70) {
+      medal = 'medal medal2'
+    }
+    if (achives > 80) {
+      medal = 'medal medal1'
+    }
+    return (
+      <>
+        <div className='award'>
+          <h6 className='score' >{score}</h6>
+          <span className={note}>{notes}</span>
+          <div className={medal}></div>
+        </div>
+        <button
+          type='button'
+          className='finish button-main'
+          onClick={() => {
+            setIsStart(!isStart)
+            setComplete(!complete)
+            achive = 0
+          }}
+        >Finish</button>
+        <div className='frame'>
+        </div>
+      </>
+    )
+  }
+
+  // Component Game
+  function Game() {
+    const next = '>>'
+    const [result, setResult] = useState('Chính xác')
+    const [question, setQuestion] = useState(`20 ${questionOperator === 'random' ? '+' : questionOperator} 5 = `)
+    const [answer, setAnswer] = useState(eval(`20 ${questionOperator === 'random' ? '+' : questionOperator} 5`))
+    const [count, setCount] = useState(0)
+    const [input, setInput] = useState(eval(`20 ${questionOperator === 'random' ? '+' : questionOperator} 5`))
+    const [timer, setTimer] = useState(questionTime)
+    const [disabled, setDisabled] = useState(true)
+    const handleClick = (value) => {
+      switch (value) {
+        case 'C':
+          if (input.length == 1) {
+            setInput('0')
+            return
+          }
+          setInput(input.slice(0, -1));
+          break
+        case '>>':
+          if (count > 9) {
+            setComplete(!complete)
+            setAchives(achive)
+          } else {
+            let element_1 = Math.ceil(20 * Math.random())
+            let element_2 = Math.ceil(20 * Math.random())
+            let operator
+            const operatorArr = ['+', '-', '*', '/']
+            if (questionOperator === 'random') {
+              operator = operatorArr[Math.floor(4 * Math.random())]
+            } else {
+              operator = questionOperator
+            }
+
+            if (operator === '/') {
+              if (element_1 % element_2 != 0) {
+                element_1 = element_2 * Math.ceil(20 * Math.random())
+              }
+            }
+            if (operator === '-') {
+              if (element_1 < element_2) {
+                element_1 = element_2 + Math.ceil(50 * Math.random())
+              }
+            }
+            let question = `${element_1} ${operator} ${element_2} =`
+            // if (result === 'Biểu thức chính xác') {
+            //   achive = achive + 10;
+            // }
+            setQuestion(question)
+            setCount(prev => prev + 1)
+            setResult('Nhập đáp án')
+            setInput('0')
+            setAnswer('')
+            setTimer(questionTime)
+            setDisabled(false)
+          }
+          break
+        default:
+          setInput(input === '0' ? value : input + value)
+          break
+      }
+    }
+
+    const handleSubmitClick = () => {
+      const arrMath = question.split('=')
+      setAnswer(eval(arrMath[0]))
+      if (eval(arrMath[0]) != input) {
+        setResult('Biểu thức sai')
+      } else {
+        setResult('Biểu thức chính xác')
+        achive = achive + 10
+      }
+      setDisabled(true)
+    }
+
+    const handleDelete = () => {
+      setInput('0')
+    }
+
+    // Timer.
+    useEffect(() => {
+      const countTimer = setInterval(() => {
+        setTimer(prev => prev - 1)
+      }, 1000)
+
+      const outTimer = setTimeout(() => {
+        handleClick('>>')
+      }, questionTime * 1000)
+
+      return () => {
+        clearInterval(countTimer)
+        clearTimeout(outTimer)
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [count])
+    const conclude = `${question} ${answer} `
+    return (
+      <>
+        <h5 className='count'>{count == 0 ? 'Ví dụ mẫu' : `Câu hỏi số ${count} `}</h5>
+        <h6 className="result" style={{ color: result == 'Biểu thức sai' ? 'red' : result === 'Biểu thức chính xác' ? 'green' : 'black' }} >{result}</h6>
+        <div className="calculator" id='calculator' >
+          <div className='input' style={{ color: result == 'Biểu thức sai' ? 'red' : result === 'Biểu thức chính xác' ? 'green' : 'black' }}>
+            {conclude}
+          </div>
+          <div className="answer">{input} <span onClick={handleDelete}>&times;</span></div>
+          <div className='row'>
+            <p className='achive'>{`Điểm: ${achive} /100`}</p>
+            <span className='time'>{`Thời gian: ${timer} giây`}</span>
+          </div>
+          <div className="button-panel">
+            <button className='item1' onClick={() => handleClick('1')}>1</button>
+            <button className='item2' onClick={() => handleClick('2')}>2</button>
+            <button className='item3' onClick={() => handleClick('3')}>3</button>
+            <button className='item4' onClick={() => handleClick('4')}>4</button>
+            <button className='item5' onClick={() => handleClick('5')}>5</button>
+            <button className='itemNext next' onClick={() => handleClick('>>')} >{next}</button>
+            <button className='item6' onClick={() => handleClick('6')}>6</button>
+            <button className='item7' onClick={() => handleClick('7')}>7</button>
+            <button className='item8' onClick={() => handleClick('8')}>8</button>
+            <button className='item9' onClick={() => handleClick('9')}>9</button>
+            <button className='item0' onClick={() => handleClick('0')}>0</button>
+            <button className='itemC' onClick={() => handleClick('C')}>C</button>
+          </div>
+          <button className='button-main' type='submit' onClick={handleSubmitClick} disabled={disabled}>SUBMIT</button>
+        </div>
+        <div className='frame'>
+        </div>
+      </>
+
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!isStart ? <Home></Home> : (complete ? <Complete></Complete> : <Game></Game>)}
+    </>
   );
 }
 
